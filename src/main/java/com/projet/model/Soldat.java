2e95fcb3226a3893ba1b3f32124e4b53c0848e27
Reight123 <clement_praud@yahoo.com>
2024-12-20 17:37:19 +0100

    :sparkles: ajout modele

--- src/main/java/com/projet/model/Soldat.java
+++ src/main/java/com/projet/model/Soldat.java
@@ -1,4 +1,66 @@
 package com.projet.model;
 
 public class Soldat {
+    private int positionX;
+    private int positionY;
+    private int pointsDefense;
+    private boolean blesse;
+    private int coutProduction;
+
+    public Soldat(int positionX, int positionY, int pointsDefense, int coutProduction) {
+        this.positionX = positionX;
+        this.positionY = positionY;
+        this.pointsDefense = pointsDefense;
+        this.blesse = false;
+        this.coutProduction = coutProduction;
+    }
+
+    // Getters et Setters
+    public int getPositionX() {
+        return positionX;
+    }
+
+    public void setPositionX(int positionX) {
+        this.positionX = positionX;
+    }
+
+    public int getPositionY() {
+        return positionY;
+    }
+
+    public void setPositionY(int positionY) {
+        this.positionY = positionY;
+    }
+
+    public int getPointsDefense() {
+        return pointsDefense;
+    }
+
+    public void setPointsDefense(int pointsDefense) {
+        this.pointsDefense = pointsDefense;
+    }
+
+    public boolean isBlesse() {
+        return blesse;
+    }
+
+    public void setBlesse(boolean blesse) {
+        this.blesse = blesse;
+    }
+
+    public int getCoutProduction() {
+        return coutProduction;
+    }
+
+    public void setCoutProduction(int coutProduction) {
+        this.coutProduction = coutProduction;
+    }
+
+    // Méthodes
+    public void soigner() {
+        if (blesse) {
+            this.blesse = false;
+            this.pointsDefense += 10; // Règle : augmenter la défense lors de la guérison
+        }
+    }
 }
